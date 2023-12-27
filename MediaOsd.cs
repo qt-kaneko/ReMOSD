829578c97398b1687bf3c62e40c409f8f3b06e9d
Kaneko Qt <mail.qt.kaneko@gmail.com>
2023-12-28 00:41:29 +0200

    Algorithm changes | Refactoring

---
+++ MediaOsd.cs
@@ -0,0 +1,57 @@
+﻿using System.ComponentModel;
+using System.Runtime.InteropServices;
+using Windows.Win32.Foundation;
+using Windows.Win32.Graphics.Gdi;
+using static Windows.Win32.System.SystemServices.APPCOMMAND_ID;
+using static Windows.Win32.PInvoke;
+using System;
+using System.Threading;
+
+class MediaOsd
+{
+  const string _className = "NativeHWNDHost\0";
+
+  public HWND HWnd { get; private set; }
+
+  public HRGN Region
+  {
+    set {
+      var result = SetWindowRgn(HWnd, value, true);
+      if (result == 0) throw new Win32Exception(Marshal.GetLastWin32Error());
+    }
+  }
+
+  public uint Dpi
+  {
+    get {
+      var dpi = GetDpiForWindow(HWnd);
+      if (dpi == 0) throw new Win32Exception(Marshal.GetLastWin32Error());
+
+      return dpi;
+    }
+  }
+
+  MediaOsd() {}
+
+  public static MediaOsd Find()
+  {
+    var osd = new MediaOsd();
+
+    // Trigger show volume control window so it is created and can be found later
+    // (to pack APPCOMMAND lParam https://stackoverflow.com/a/29301152/18449435)
+    // (https://forums.codeguru.com/showthread.php?147192-How-to-construct-WM_APPCOMMAND-message)
+    SendMessage(GetShellWindow(), WM_APPCOMMAND, 0, (int)APPCOMMAND_VOLUME_MUTE << 16);
+    SendMessage(GetShellWindow(), WM_APPCOMMAND, 0, (int)APPCOMMAND_VOLUME_MUTE << 16);
+
+    for (var attempt = 1; attempt <= 5; ++attempt)
+    {
+      osd.HWnd = FindWindowEx(default, default, _className, default);
+      if (osd.HWnd != default) break;
+
+      Thread.Sleep(250);
+    }
+    if (osd.HWnd == default) throw new Exception("Media OSD was not found :(");
+
+    return osd;
+  }
+}
--- Program.cs
+++ Program.cs
@@ -1,73 +1,54 @@
-﻿// This file is part of ReMOSD.
-//
-// ReMOSD is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-//
-// ReMOSD is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License along with ReMOSD. If not, see <https://www.gnu.org/licenses/>.
-// Copyright 2022 Kaneko Qt
-
-using System;
+﻿using System;
 using System.ComponentModel;
 using System.Drawing;
 using System.Linq;
 using System.Runtime.InteropServices;
-
 using Windows.Win32.Graphics.Gdi;
-
 using static Windows.Win32.PInvoke;
 
-static class Program {
-  static readonly Size _miniOsdSize = new(65, 140);
+Size _miniOsdSize = new(65, 140);
 
-  static void Main(string[] args) {
-    AppDomain.CurrentDomain.UnhandledException += OnUnhandledException;
+AppDomain.CurrentDomain.UnhandledException += OnUnhandledException;
 
-    WindowsMediaOsd osd = WindowsMediaOsd.Find();
+var osd = MediaOsd.Find();
 
-    HRGN newOsdRegion;
-    if (args.Contains("--restore") || args.Contains("-r")) {
-      newOsdRegion = default;
-    }
-    else {
-      float osdScalingCoefficient = osd.Dpi / 96.0f;
+HRGN newOsdRegion;
+if (args.Contains("--restore") || args.Contains("-r")) newOsdRegion = new HRGN();
+else
+{
+  var osdScalingCoefficient = osd.Dpi / 96.0f;
 
-      newOsdRegion = CreateRectRgn(0, 0,
-                                   (int)Math.Round(_miniOsdSize.Width * osdScalingCoefficient),
-                                   (int)Math.Round(_miniOsdSize.Height * osdScalingCoefficient));
-      if (newOsdRegion == default)
-        throw new Win32Exception(Marshal.GetLastWin32Error());
-    }
+  newOsdRegion = CreateRectRgn(0, 0,
+                               (int)Math.Round(_miniOsdSize.Width * osdScalingCoefficient),
+                               (int)Math.Round(_miniOsdSize.Height * osdScalingCoefficient));
+  if (newOsdRegion == default) throw new Win32Exception(Marshal.GetLastWin32Error());
+}
 
-    osd.Region = newOsdRegion;
-  }
+osd.Region = newOsdRegion;
 
-  static void OnUnhandledException(object sender, UnhandledExceptionEventArgs e) {
-    Exception exception = e.ExceptionObject as Exception;
+static void OnUnhandledException(object s, UnhandledExceptionEventArgs e)
+{
+  var exception = e.ExceptionObject as Exception;
 
-    AllocConsole();
+  AllocConsole();
+  Console.Title = "ReMOSD";
 
-    Console.Title = "ReMOSD";
+  Console.WriteLine(exception);
+  Console.WriteLine();
 
-    Console.WriteLine(exception);
+  Console.WriteLine("""
+                    +----------------------------------------------+
+                    | PLEASE CONSIDER TO SEND A COPY OF TEXT ABOVE |
+                    | OR SCREENSHOT WITH THIS WINDOW TO DEVELOPER. |
+                    |                                              |
+                    | Telegram: @qt_kaneko                         |
+                    | GitHub:   github.com/qt-kaneko/remosd        |
+                    +----------------------------------------------+
+                    """);
+  Console.WriteLine();
 
-    Console.WriteLine();
+  Console.WriteLine("Press any key to close this window . . .");
+  Console.ReadKey(true);
 
-    Console.WriteLine("""
-                      +----------------------------------------------+
-                      | PLEASE CONSIDER TO SEND A COPY OF TEXT ABOVE |
-                      | OR SCREENSHOT WITH THIS WINDOW TO DEVELOPER. |
-                      |                                              |
-                      | Telegram: @qt_kaneko                         |
-                      | GitHub:   github.com/qt-kaneko/remosd        |
-                      +----------------------------------------------+
-                      """);
-
-    Console.WriteLine();
-
-    Console.WriteLine("Press any key to close this window . . .");
-    Console.ReadKey(true);
-
-    Environment.Exit(Marshal.GetHRForException(exception));
-  }
+  Environment.Exit(Marshal.GetHRForException(exception));
 }
