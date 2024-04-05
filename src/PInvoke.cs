6e7d02953e4d0c15e295c6c7f64d65f867d8df4a
Kaneko Qt <mail.qt.kaneko@gmail.com>
2024-04-05 04:21:43 +0300

    Minor refactoring

--- src/PInvoke.cs
+++ src/PInvoke.cs
@@ -24,34 +24,34 @@ static class PInvoke
   public const int APPCOMMAND_VOLUME_MUTE = 8;
 
   /// <summary> https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-getwindowrgnbox </summary>
-  [DllImport("user32.dll", SetLastError = true)]
+  [DllImport("user32.dll", SetLastError = true, ExactSpelling = true, BestFitMapping = false, ThrowOnUnmappableChar = true)]
   public static extern int GetWindowRgnBox(nint hWnd, out RECT lprc);
 
   /// <summary> https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-setwindowrgn </summary>
-  [DllImport("user32.dll", SetLastError = true)]
+  [DllImport("user32.dll", SetLastError = true, ExactSpelling = true, BestFitMapping = false, ThrowOnUnmappableChar = true)]
   public static extern int SetWindowRgn(nint hWnd, nint hRgn, bool bRedraw);
 
   /// <summary> https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-getdpiforwindow </summary>
-  [DllImport("user32.dll", SetLastError = true)]
+  [DllImport("user32.dll", SetLastError = true, ExactSpelling = true, BestFitMapping = false, ThrowOnUnmappableChar = true)]
   public static extern uint GetDpiForWindow(nint hWnd);
 
   /// <summary> https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-getshellwindow </summary>
-  [DllImport("user32.dll", SetLastError = true)]
+  [DllImport("user32.dll", SetLastError = true, ExactSpelling = true, BestFitMapping = false, ThrowOnUnmappableChar = true)]
   public static extern nint GetShellWindow();
 
   /// <summary> https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-sendmessage </summary>
-  [DllImport("user32.dll", SetLastError = true)]
-  public static extern nint SendMessage(nint hWnd, uint Msg, nint wParam, nint lParam);
+  [DllImport("user32.dll", SetLastError = true, ExactSpelling = true, BestFitMapping = false, ThrowOnUnmappableChar = true)]
+  public static extern nint SendMessageA(nint hWnd, uint Msg, nint wParam, nint lParam);
 
   /// <summary> https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-findwindowa </summary>
-  [DllImport("user32.dll", SetLastError = true)]
+  [DllImport("user32.dll", SetLastError = true, ExactSpelling = true, BestFitMapping = false, ThrowOnUnmappableChar = true)]
   public static extern nint FindWindowA(string? lpClassName, string? lpWindowName);
 
   /// <summary> https://learn.microsoft.com/en-us/windows/win32/api/wingdi/nf-wingdi-createrectrgn </summary>
-  [DllImport("gdi32.dll", SetLastError = true)]
+  [DllImport("gdi32.dll", SetLastError = true, ExactSpelling = true, BestFitMapping = false, ThrowOnUnmappableChar = true)]
   public static extern nint CreateRectRgn(int x1, int y1, int x2, int y2);
 
   /// <summary> https://learn.microsoft.com/en-us/windows/console/allocconsole </summary>
-  [DllImport("kernel32.dll", SetLastError = true)]
+  [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true, BestFitMapping = false, ThrowOnUnmappableChar = true)]
   public static extern bool AllocConsole();
 }
